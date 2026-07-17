b41bf6a433e6fc37f3958b48a6f6afa25d38aca4
karike uday krushna <karikeudaykrushna@udays-MacBook-Air.local>
2026-07-17 13:35:51 +0530

    Swapping of two elements

---
+++ Swap.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Swap {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a=sc.nextInt();
+        System.out.println("Enter the second number");
+        int b=sc.nextInt();
+        System.out.println("Before swapping: a = " + a + ", b = " +
+        b);
+        a=a+b-(b=a);
+        System.out.println("After swapping: a = " + a + ", b = " +b);
+        sc.close();
+
+    }
+}
